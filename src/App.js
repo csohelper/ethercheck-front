cd1f6997d5d4da99e3442e6663995dc333f919d7
Merijn <egor.sobolev.0606@bk.ru>
2025-11-27 21:54:01 +0300

    feat: Удаление ненужных файлов и настройка компонента App (master)
    Удалены файлы App.test.js, reportWebVitals.js, setupTests.js.
    Внесены изменения в App.js:
    - Удален импорт reportWebVitals.
    - Добавлен компонент MyButton.
    - Добавлен запрос к API и вывод ответа в DOM.
    - Добавлен state для хранения ответа API.
    - Используется useEffect для загрузки данных при монтировании компонента.
    
    Добавлен новый файл MyButton.jsx.
    Добавлен .idea/ в .gitignore.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MyButton from './MyButton';
+
+const restEndpoint =
+    "https://monitor.slavapmk.ru/api/data?start=2025-11-27%2013:25&end=2025-11-27%2021:25&rooms=total";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        const load = async () => {
+            const res = await fetch(restEndpoint);
+            const t = await res.text();   // ← вот это даёт строку
+            setText(t);
+        };
+
+        load();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <MyButton name="jhguyuygh" />
+                <div>{text}</div>  {/* ← сюда вставляется весь текст ответа */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/MyButton.jsx
@@ -0,0 +1,7 @@
+function MyButton({name}) {
+    return (
+        <button className="App-button">{name}</button>
+    )
+}
+
+export default MyButton;
